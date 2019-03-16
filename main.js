b7cde6cdbffd1d7e2a840891587c761258d280c6
Reza Madhavan <48611644+rezamadhavan@users.noreply.github.com>
2019-03-16 18:30:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ if(requestComplete = true){
     console.log(info)
 }
 
-function call_Nasa(search, location, startDate, endDate){
+function call_Nasa(search, location='', startDate='', endDate=''){
   var url = initialUrl + "q=" + search //+ "&location=" + location + "&year_start=" + startDate + "&year_end=" + endDate
   
   fetch(url)
   
   .then(function(data)){
     info = (res => res.JSON()))
+    console.log(data)
     requestComplete = true
     }
 
 .catch(function(error) {
-    // If there is any error you will catch them here
+    console.log("Error!")
   });   
   
   
