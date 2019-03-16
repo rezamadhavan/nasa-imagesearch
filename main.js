c0d270151f9f54e4d87b384a26973b08f3b8629c
Reza Madhavan <48611644+rezamadhavan@users.noreply.github.com>
2019-03-16 11:46:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+var info = {}
+var requestComplete = false
+const initialUrl = 'https://images-api.nasa.gov/search?'
 
+if(requestComplete = true){
+    console.log(info)
+}
+
+function call_Nasa(search, location, startDate, endDate){
+  var url = initialUrl + "q=" + search //+ "&location=" + location + "&year_start=" + startDate + "&year_end=" + endDate
+  
+  fetch(url)
+  
+  .then(function(data)){
+    info = (res => res.JSON()))
+    requestComplete = true
+    }
+
+.catch(function(error) {
+    // If there is any error you will catch them here
+  });   
+  
+  
+  
+  
+  
+}
